50c5a69506c72ab8616f4d7807ea9f079fdbd6b0
Adrián Pardini <github@tangopardo.com.ar>
2017-03-25 18:58:49 -0300

    list_devices: use DeviceMonitor

--- list_devices.py
+++ list_devices.py
@@ -10,46 +10,41 @@ from gi.repository import Gst, GLib, GObject
 GObject.threads_init()
 Gst.init(sys.argv)
 
+from devices import DeviceMonitor
+
 DEVICE_NAME_WIDTH = 74
 DISPLAY_FORMAT = '{0:%i} {1}' % DEVICE_NAME_WIDTH
 
-monitor = Gst.DeviceMonitor.new()
-monitor.add_filter('Audio/Source')
+def display_device(device):
+    print(DISPLAY_FORMAT.format(device.internal_name, device.display_name))
 
-def bus_element_cb (bus, msg, arg=None):
-    s = msg.get_structure()
-    if s is None:
-        return True
+def on_device_added(monitor, device):
+    print('Device added:')
+    display_device(device)
 
-    name = s.get_name()
-    if name not in ['GstMessageDeviceAdded', 'GstMessageDeviceRemoved']:
-        return True
+def on_device_removed(monitor, device):
+    print('Device removed:')
+    display_device(device)
 
-    name_map = {
-        'GstMessageDeviceAdded':   'Device added:',
-        'GstMessageDeviceRemoved': 'Device removed:'
-    }
-    device = s.get_value('device')
-    print(name_map[name], DISPLAY_FORMAT.format(device.props.internal_name, device.props.display_name))
-    return True
 
 if __name__ == '__main__':
+    device_monitor = DeviceMonitor()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--monitor', action='store_true', help='Keep listening for device changes')
 
-    devices = monitor.get_devices()
+    devices = device_monitor.get_devices()
     for device in devices:
-        print(DISPLAY_FORMAT.format(device.props.internal_name, device.props.display_name))
+        display_device(device)
 
     args = parser.parse_args()
     if args.monitor:
         import signal
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-        bus = monitor.get_bus()
-        bus.add_signal_watch()
-        bus.connect("message", bus_element_cb)
-        monitor.start()
+        device_monitor.connect('device-added',   on_device_added)
+        device_monitor.connect('device-removed', on_device_removed)
+        device_monitor.start()
 
         loop = GLib.MainLoop()
         loop.run()
